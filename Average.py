cd1862f0d8e70f44b0e030b61320b7c5ac405e76
SanjuMu <sanjusri.munigala@gmail.com>
2024-11-13 19:29:12 -0500

    Average

---
+++ Average.py
@@ -0,0 +1,11 @@
+tree1= 30
+tree2= 60
+tree3= 100
+tree4= 91
+tree5= 97
+
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all 5 trees is:", sum)
+
+average= sum/5
+print ("the average height of all 5 trees is", average)
